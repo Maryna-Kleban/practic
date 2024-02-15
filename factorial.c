b68c1a1253266d3c90ce7f48b9f60d4970604e96
Maryna-Kleban <maryna.kleban.20@pnu.edu.ua>
2024-02-15 21:16:35 +0200

    added function for sum

--- factorial.c
+++ factorial.c
@@ -8,6 +8,14 @@ int factorial(int n) {
     }
 }
 
+int sum(int n) {
+    int result = 0;
+    for (int i = 1; i <= n; i++) {
+        result += i;
+    }
+    return result;
+}
+
 int main() {
     int num;
     printf("Enter a number: ");
