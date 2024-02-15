9791979d05762c7975150dc69ff5634db00e3040
Maryna-Kleban <maryna.kleban.20@pnu.edu.ua>
2024-02-15 21:23:59 +0200

    added function for sum

--- factorial.c
+++ factorial.c
@@ -16,10 +16,30 @@ int sum(int n) {
     return result;
 }
 
+int is_prime(int n) {
+    if (n <= 1) {
+        return 0; // Не є простим
+    }
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return 0; // Не є простим
+        }
+    }
+    return 1; // Є простим
+}
+
 int main() {
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
     printf("Factorial of %d is %d\n", num, factorial(num));
+    printf("Sum of numbers from 1 to %d is %d\n", num, sum(num));
+
+    if (is_prime(num)) {
+        printf("%d is a prime number\n", num);
+    } else {
+        printf("%d is not a prime number\n", num);
+    }
+
     return 0;
 }
